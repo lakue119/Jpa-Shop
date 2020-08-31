c6a8138752face6a15ff26460a37a6a3325e0ef5
Lakue <audfkr0808@naver.com>
2020-09-01 00:58:38 +0900

    Jpa환경설정

---
+++ src/main/java/com/jpalakue/jpashop/Hello.java
@@ -0,0 +1,9 @@
+package com.jpalakue.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Hello {
+    private String data;
+}
---
+++ src/main/java/com/jpalakue/jpashop/JpashopApplication.java
@@ -0,0 +1,18 @@
+package com.jpalakue.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopApplication {
+
+	public static void main(String[] args) {
+
+		Hello hello = new Hello();
+		hello.setData("alskejkl");
+		String data = hello.getData();
+		System.out.println(data);
+		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+}
